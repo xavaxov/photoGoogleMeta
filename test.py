5ef2823e3429879472a3d87cf4ee39677d83aa6b
XavaXov <xavaxov@gmail.com>
2023-09-05 18:03:23 +0300

    fix

--- test.py
+++ test.py
@@ -7,43 +7,39 @@ import os
 directory = "/Загрузки/Takeout/Google Фото"
  
 # Получаем список файлов
-files = os.listdir(directory)
+subdirectory = os.listdir(directory)
  
 # Выводим список файлов
-print(files)
+print(subdirectory)
 
-for name in files:
-    subfiles = os.listdir(directory + name)
+for name in subdirectory:
+    files = os.listdir(os.path.join(directory,name))
 
-    for i in range(len(subfiles)-1):
-        file_img = subfiles[i]
+    for i in range(len(files)-1):
+        file_img = files[i]
 
         if not(file_img[len(file_img)-3:] == 'jpg' and files[i+1] == file_img + '.json'):
             continue
 
-        if file_img[len(file_img)-3:] == 'jpg':
-            if subfiles[i+1] == file_img + '.json':
-                file_json = subfiles[i+1]
-                print(subfiles[i], end = ' ')
+        file_json = files[i+1]
+        print(files[i], end = ' ')
 
-                with open(os.path.join(directory,name,file_img), 'rb') as img:
-                    img = Image(img)
+        with open(os.path.join(directory,name,file_img), 'rb') as img:
+            img = Image(img)
 
-                with open(os.path.join(directory,name,file_img), 'r') as file_json:
-                    templates = json.load(file_json)
+        with open(os.path.join(directory,name,file_img), 'r') as file_json:
+            templates = json.load(file_json)
 
-                timestamp_file = int(templates['photoTakenTime']['timestamp']) - 3*60*60
-               
-                time_file_str = datetime.fromtimestamp(timestamp_file).strftime('%Y:%m:%d %H:%M:%S')
-                print(time_file_str)
+        timestamp_file = int(templates['photoTakenTime']['timestamp']) - 3*60*60
+        
+        time_file_str = datetime.fromtimestamp(timestamp_file).strftime('%Y:%m:%d %H:%M:%S')
+        print(time_file_str)
 
-                try:
-                    img.delete_all()
-                    img.datetime_original = time_file_str
-                    with open(directory + name + '/'  + file_img, 'wb') as updated_img:
-                        updated_img.write(img.get_file())
-
-                except:
-                    print('\n\n--- bug --- \n' + file_img, end = '\n\n')
-
+        try:
+            img.delete_all()
+            img.datetime_original = time_file_str
+            with open(os.path.join(directory,name,file_img), 'wb') as updated_img:
+                updated_img.write(img.get_file())
                 
+        except:
+            print('\n\n--- bug --- \n' + file_img, end = '\n\n')
